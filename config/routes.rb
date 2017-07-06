0815b9d64657005f85f9345051ed6de7825d6d1f
k_ukai <ken151102@gmail.com>
2017-07-06 04:50:01 +0000

    added session function, did modication of tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   root to: 'tasks#index'
-
+  
+  # task
   resources :tasks
   
+  # session
+  # get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  # user
   get 'signup', to: 'users#new'
   resources :users, only: [:index,:show, :new, :create]
   
